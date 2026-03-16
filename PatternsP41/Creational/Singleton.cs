d1a504dbe9b0bede2fbfebcc8576de7a9c903079
Vasyl Kovalov <raid3r81@gmail.com>
2026-03-16 21:00:33 +0200

    Добавьте файлы проекта.

---
+++ PatternsP41/Creational/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternsP41.Creational;
+
+public class  SingletonService
+{
+    // Приватний конструктор,
+    // щоб запобігти створенню об'єктів ззовні класу
+    private SingletonService() {
+        _createdAt = DateTime.Now;
+        Console.WriteLine($"SingletonService created at {_createdAt}");
+        
+    }
+
+    static SingletonService()
+    {
+        // Статичний конструктор, який ініціалізує єдиний екземпляр класу
+        _instance = new SingletonService();
+    }
+
+    private DateTime _createdAt;
+
+    private static SingletonService _instance;
+
+    public static SingletonService Instance => _instance ??= new SingletonService();
+
+    public DateTime CreatedAt => _createdAt;
+}
---
+++ PatternsP41/Program.cs
@@ -0,0 +1,67 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PatternsP41.Creational;
+
+Console.WriteLine("Hello, World!");
+
+
+
+
+/*
+ UML - Unified Modeling Language
+
+- для візуалізації, 
+- опису, 
+- конструювання 
+- документування 
+
+Види діаграм UML:
+
+- Діаграми класів (Class Diagram)
+- Діаграма варіантів використання (Use Case Diagram)
+- Діаграма активностей (Activity Diagram)
+- Діаграма послідовності (Sequence Diagram)
+
+- Діаграма станів (State Diagram)
+
+Чернетка - Нове замовлення - Офрмлено - Оплачено - Відправлено - Доставлено
+                                      - Відмінено
+
+- Діаграма компонентів (Component Diagram)
+- Діаграма розгортання (Deployment Diagram)
+- Діаграма пакетів (Package Diagram)
+
+*/
+
+/*
+Патерни проектування (Design Patterns) - 
+це повторювані рішення для загальних проблем, що виникають під час розробки програмного забезпечення. Вони надають шаблони для організації коду та взаємодії між об'єктами. 
+
+Види патернів проектування:
+- Породжуючі (Creational) - зосереджені на створенні об'єктів.
+- Структурні (Structural) - зосереджені на організації класів та об'єктів.
+- Поведінкові (Behavioral) - зосереджені на взаємодії між об'єктами та їх поведінці.
+
+
+ - Породжуючі (Creational)
+- Одиночка (Singleton)
+- Фабричний метод (Factory Method)  
+- Абстрактна фабрика (Abstract Factory)
+- Будівельник (Builder)
+- Прототип (Prototype)
+
+ */
+
+void SomeMethod()
+{
+    var singleton2 = SingletonService.Instance;
+    Console.WriteLine(singleton2.CreatedAt);
+}
+
+
+
+//var singleton1 = SingletonService.Instance;
+//Console.WriteLine(singleton1.CreatedAt);
+//Thread.Sleep(2000);
+//SomeMethod();
+
+Console.WriteLine("Press any key to exit...");
